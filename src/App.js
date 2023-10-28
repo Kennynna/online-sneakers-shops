499dbdb5d50055a45f5fcc12471922ba91b14726
Kennynna <kennynna@mail.ru>
2023-10-28 23:16:01 +0300

    Создали  компоненты товаров корзины и хедара

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Overlay from "./components/Overlay";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper clear mt-40 ">
+      {/* правое окно КОРЗИНЫ */}
+      <Overlay/>
+
+
+      <Header/>
+
+
+
+    {/* content */}
+
+
+      {/* Sneakers*/}
+      <div className=" content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>Все кросcовки</h1>
+        <div className="search-block d-flex">
+          <img src="/img/search.svg" alt="Search"/>
+          <input placeholder="Поиск..."/>
+        </div>
+      </div>
+
+      <div className="mainCards d-flex">
+      {/* Карточка товара на главной странице */}
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+      </div>
     </div>
+      </div>
   );
 }
 
